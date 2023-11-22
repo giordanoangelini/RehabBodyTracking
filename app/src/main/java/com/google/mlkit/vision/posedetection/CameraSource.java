218350dc65b880c7eb13a30059bc8302eb3d1b47
Giordano <angelinigiordano@gmail.com>
2023-11-22 13:52:26 +0100

    removed shared prefs

--- app/src/main/java/com/google/mlkit/vision/posedetection/CameraSource.java
+++ app/src/main/java/com/google/mlkit/vision/posedetection/CameraSource.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 Google LLC. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.mlkit.vision.posedetection;
 
 import android.Manifest;
@@ -298,14 +282,11 @@ public class CameraSource {
     }
     Camera camera = Camera.open(requestedCameraId);
 
-    SizePair sizePair = PreferenceUtils.getCameraPreviewSizePair(activity, requestedCameraId);
-    if (sizePair == null) {
-      sizePair =
+    SizePair sizePair =
           selectSizePair(
               camera,
               DEFAULT_REQUESTED_CAMERA_PREVIEW_WIDTH,
               DEFAULT_REQUESTED_CAMERA_PREVIEW_HEIGHT);
-    }
 
     if (sizePair == null) {
       throw new IOException("Could not find suitable preview size.");
